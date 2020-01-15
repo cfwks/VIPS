231a9ad7162c7c4ef2d3b67a73e004d96db7032b
mami <51700728+gorigorillachan@users.noreply.github.com>
2020-01-15 15:50:01 +0900

    Add files via upload

---
+++ cart/cart.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/cart.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+    </head>
+
+    <body>
+
+    <div id="header">
+
+        <p><img src="images/logo.png" alt="vips アイコン"></p><h1>商品カート</h1>
+
+
+    </div>
+    
+
+
+
+    <div id="cart">
+        
+
+    <p>小計（〇点）：￥ｘｘ、ｘｘｘ円</p>
+
+
+    <div id="button_buy">
+    <a href="">
+    <button type="button">レジに進む</button>
+</a>
+</div>
+    
+
+    <div class="item">
+        <p><img src="images/item.jpg" alt="商品の写真"></p>
+        
+            <table>
+                <tr><th>商品名</th><td>ｘｘｘ</td></tr>
+                <tr><th>価　格</th><td>￥ｘｘ、ｘｘｘ</td></tr>
+            </table>
+            
+    
+        
+    </div>
+
+    <div id="back_button">
+    <a href=""><button type="button">前のページに戻る</button></a>
+</div>
+    </div>
+
+
+
+
+    </body>
+
+
+
+
+</html>

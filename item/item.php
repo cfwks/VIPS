4a009cb02d8da2ae7355bdc8494fb7e8c17a7628
mami <51700728+gorigorillachan@users.noreply.github.com>
2020-01-15 15:47:51 +0900

    Add files via upload

---
+++ item/item.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>商品詳細ページ</title>
+        <link rel="stylesheet" type="text/css" href="css/item.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+    </head>
+
+
+    <body>
+    <div id="header">
+
+        <p><img src="images/logo.png" alt="vips アイコン"></p><h1>商品詳細</h1>
+
+    </div>
+
+
+    <div id="item">
+            
+        
+        <div id="item_left">
+            <p><img src="images/item.jpg" alt="商品の画像"></p>
+            <a href=""><button type="button">いいね！</button></a>
+            <a href=""><button type="button">コメント</button></a>
+        </div>
+
+        <div id="item_con">
+           
+                <div id="item_name">
+                    <p>韓国風ワンピース</p>
+                </div>    
+
+                <div id="item_disc">
+
+                    <table>
+                        <tr><th>カテゴリー</th><td>aaa</td></tr>
+                        <tr><th>サイズ</th><td>aaa</td></tr>
+                        <tr><th>ブランド</th><td>aaa</td></tr>
+                        <tr><th>商品の状態</th><td>aaa</td></tr>
+                        <tr><th>発送料の負担</th><td>aaa</td></tr>
+                        <tr><th>発送元の地域</th><td>aaa</td></tr>
+                    </table>
+
+                </div>
+
+        <a href=""><button type="button">購入する</button></a>
+        
+        </div>
+
+        
+        
+
+    </div>
+
+
+    <div id="back_button">
+            <a href=""><button type="button">前のページに戻る</button></a>
+        </div>
+
+
+
+    </body>
+</html>
